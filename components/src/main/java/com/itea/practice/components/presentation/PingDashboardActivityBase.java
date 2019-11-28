661f4f425d6b029df1b7888a4433fcd0bfc7b2e3
Volodymyr <v.dranchuk@boosteroid.com>
2019-11-28 22:14:50 +0200

    Added xml

--- components/src/main/java/com/itea/practice/components/presentation/PingDashboardActivityBase.java
+++ components/src/main/java/com/itea/practice/components/presentation/PingDashboardActivityBase.java
@@ -1,13 +1,10 @@
 package com.itea.practice.components.presentation;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.annotation.CallSuper;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.itea.practice.components.R;
 
@@ -15,40 +12,22 @@ import java.util.Objects;
 
 public abstract class PingDashboardActivityBase extends AppCompatActivity {
 
-    private final View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if (view.equals(btnStats)) PingDashboardActivityBase.this.showStats();
-            if (view.equals(btnRun)) PingDashboardActivityBase.this.startPingService();
-            if (view.equals(btnStop)) PingDashboardActivityBase.this.stopPingService();
-        }
-    };
+//    private final View.OnClickListener clickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            if (view.equals(btnStats)) PingDashboardActivityBase.this.showStats();
+//            if (view.equals(btnRun)) PingDashboardActivityBase.this.startPingService();
+//            if (view.equals(btnStop)) PingDashboardActivityBase.this.stopPingService();
+//        }
+//    };
 
-    private View btnStats;
-    private View btnRun;
-    private View btnStop;
-    private TextView outputStatus;
+//    private View btnStats;
+//    private View btnRun;
+//    private View btnStop;
+//    private TextView outputStatus;
 
     private void update(boolean isPingRunning) {
-        this.btnRun.setEnabled(!isPingRunning);
-        this.btnStop.setEnabled(isPingRunning);
 
-        this.outputStatus.setText(
-                getString(
-                        isPingRunning
-                                ? R.string.status_active
-                                : R.string.status_inactive
-                )
-        );
-
-        this.outputStatus.setTextColor(
-                ContextCompat.getColor(
-                        this,
-                        isPingRunning
-                                ? R.color.highlight_positive
-                                : R.color.highlight_negative
-                )
-        );
     }
 
     protected abstract void showStats();
@@ -75,21 +54,21 @@ public abstract class PingDashboardActivityBase extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.title);
     }
 
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        this.btnStats = this.findViewById(R.id.btn_stats);
-        this.btnStats.setOnClickListener(this.clickListener);
-
-        this.btnRun = this.findViewById(R.id.btn_run);
-        this.btnRun.setOnClickListener(this.clickListener);
-
-        this.btnStop = this.findViewById(R.id.btn_stop);
-        this.btnStop.setOnClickListener(this.clickListener);
-
-        this.outputStatus = this.findViewById(R.id.output_status);
-    }
+//    @Override
+//    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//
+//        this.btnStats = this.findViewById(R.id.btn_stats);
+//        this.btnStats.setOnClickListener(this.clickListener);
+//
+//        this.btnRun = this.findViewById(R.id.btn_run);
+//        this.btnRun.setOnClickListener(this.clickListener);
+//
+//        this.btnStop = this.findViewById(R.id.btn_stop);
+//        this.btnStop.setOnClickListener(this.clickListener);
+//
+//        this.outputStatus = this.findViewById(R.id.output_status);
+//    }
 
     @Override
     protected void onResume() {
