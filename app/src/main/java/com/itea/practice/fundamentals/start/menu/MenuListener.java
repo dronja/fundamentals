f4d6bbb87be31f8b1ee135e43da06aff7d584fee
Ihor Martyniuk <devmartyniuk@gmail.com>
2019-12-03 18:09:43 +0200

    Cleanup

--- app/src/main/java/com/itea/practice/fundamentals/start/menu/MenuListener.java
+++ app/src/main/java/com/itea/practice/fundamentals/start/menu/MenuListener.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.itea.practice.fundamentals.PingDashboardActivity;
-
 public final class MenuListener implements View.OnClickListener {
 
     @Override
