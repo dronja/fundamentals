69b0eb6db67e00c7561d5feb5140ff03c6323014
Volodymyr <v.dranchuk@boosteroid.com>
2019-12-03 20:52:55 +0200

    Added pingDashboard activity

---
+++ app/src/main/java/com/itea/practice/fundamentals/task/PingDashboardActivity.java
@@ -0,0 +1,13 @@
+package com.itea.practice.fundamentals.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PingDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
